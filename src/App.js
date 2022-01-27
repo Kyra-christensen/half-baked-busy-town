392455f1da964b889a71d32ee68b15ef405b86c6
Kyra Christensen <christensen.kyral@gmail.com>
2022-01-26 16:23:27 -0800

    added eventlisteners to each vehiclelist button

--- src/App.js
+++ src/App.js
@@ -52,17 +52,17 @@ function App() {
       This prop should be an array of strings like ['car', 'truck', 'truck', 'car', 'bus'].
       Do you have something like that in state that you could pass as a vehicles prop? 
       */}
-      <VehicleList />
+      <VehicleList vehicle={vehiclesArray}/>
       <div className='buttons'>
         {/* This part is weird */}
         {/* On click, you should set the traffic in state to be a copy of the same array that's already in state, but immutably add a 'car' to the end */}
-        <button>Car</button>
+        <button onClick={() => setVehiclesArray([...vehiclesArray, 'car'])}>Car</button>
         {/* On click, you should set the traffic in state to be a copy of the same array that's already in state, but immutably add a 'bus' to the end */}
-        <button>Bus</button>
+        <button onClick={() => setVehiclesArray([...vehiclesArray, 'bus'])}>Bus</button>
         {/* On click, you should set the traffic in state to be a copy of the same array that's already in state, but immutably add a 'truck' to the end */}
-        <button>Truck</button>
+        <button onClick={() => setVehiclesArray([...vehiclesArray, 'truck'])}>Truck</button>
         {/* On click, you should set the traffic in state to be a copy of the same array that's already in state, but immutably add a 'motorcycle' to the end */}
-        <button>Motorcycle</button>
+        <button onClick={() => setVehiclesArray([...vehiclesArray, 'motorcycle'])}>Motorcycle</button>
       </div>
 
     </div>
